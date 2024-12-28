0b142eb9661f2fcb1a0da45444448750bf56fcc2
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-12-28 20:05:33 +0600

    end

--- apps/main/models.py
+++ apps/main/models.py
@@ -23,3 +23,24 @@ class Settings_All(models.Model):
     class Meta:
         verbose_name = ''
         verbose_name_plural = 'Статистика в Главном Странице'
+
+class News(models.Model):
+    image = models.ImageField(upload_to='news', verbose_name='Фото')
+    title = models.CharField(max_length=155,verbose_name='Заголовок')
+    title2 = models.CharField(max_length=155,verbose_name='Заголовок 2')
+    title3 = models.CharField(max_length=155,verbose_name='Заголовок 3')
+    text1 = models.TextField(verbose_name='Описание 1')
+    text2 = models.TextField(verbose_name='Описание 2')
+
+    image2 = models.ImageField(upload_to='news', verbose_name='Фото 2')
+    title4  = models.CharField(max_length=155,verbose_name='Заголовок 4')
+    title5 = models.CharField(max_length=155,verbose_name='Заголовок 5')
+    title6 = models.CharField(max_length=155,verbose_name='Заголовок 6')
+    text3 = models.TextField(verbose_name='Описание 3')
+    text4 = models.TextField(verbose_name='Описание 4')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Новости'
